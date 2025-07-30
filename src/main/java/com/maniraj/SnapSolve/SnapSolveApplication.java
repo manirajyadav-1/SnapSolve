2057bb704f4b2fe9049ae3083a9ca70f201e1902
manirajyadav-1 <yadavmani41@gmail.com>
2025-07-30 23:40:31 +0530

    first commit

---
+++ src/main/java/com/maniraj/SnapSolve/SnapSolveApplication.java
@@ -0,0 +1,13 @@
+package com.maniraj.SnapSolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapSolveApplication.class, args);
+	}
+
+}
